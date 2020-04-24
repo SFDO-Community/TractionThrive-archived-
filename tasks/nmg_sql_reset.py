49458bbdefac34e21e0d3d98822e1629776cfeaa
Julian Joseph <julian.joseph@jjoseph-ltm2.internal.salesforce.com>
2020-04-24 01:12:11 -0700

    delete trailing spaces in tasks

--- tasks/nmg_sql_reset.py
+++ tasks/nmg_sql_reset.py
@@ -1,13 +1,13 @@
-from cumulusci.tasks.salesforce import BaseSalesforceApiTask	
+from cumulusci.tasks.salesforce import BaseSalesforceApiTask
 
-class NgmSqlReset(BaseSalesforceApiTask):	
-    def _run_task(self):	
+class NgmSqlReset(BaseSalesforceApiTask):
+    def _run_task(self):
 
-        # Resets nmg-load filed back to it's original state based on a template file	
-        with open("datasets/nmg-load-template.sql") as f:	
-            with open("datasets/nmg-load.sql", "w") as f1:	
-                for line in f:	
-                    f1.write(line)	
+        # Resets nmg-load filed back to it's original state based on a template file
+        with open("datasets/nmg-load-template.sql") as f:
+            with open("datasets/nmg-load.sql", "w") as f1:
+                for line in f:
+                    f1.write(line)
 
-        # Reset nmg-extrat to empty	
-        open('datasets/nmg-extract.sql', 'w').close() 
+        # Reset nmg-extrat to empty
+        open('datasets/nmg-extract.sql', 'w').close()
